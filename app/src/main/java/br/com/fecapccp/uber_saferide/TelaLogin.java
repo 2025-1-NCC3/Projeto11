6ee3241a5bbfa8b38838e34df22b11f7a79367d4
Guilherme Siqueira <134648341+SIkas000@users.noreply.github.com>
2025-03-19 14:25:50 -0300

    tela inicial * estrutura(aparência) * botão redireciona para a tela de login

--- app/src/main/java/br/com/fecapccp/uber_saferide/TelaLogin.java
+++ app/src/main/java/br/com/fecapccp/uber_saferide/TelaLogin.java
@@ -11,30 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class TelaLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-            });
-
-        Button btnComecar = findViewById(R.id.btnComecar);
-
-        // Configurando o clique para redirecionar para a TelaLogin
-        btnComecar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TelaLogin.class);
-                startActivity(intent);
-            }
         });
 
-    }
 
+    }
 }
