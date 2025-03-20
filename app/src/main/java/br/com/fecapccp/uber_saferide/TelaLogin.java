919fa8e9a60dcb65b55193a3b452c4441bd5e138
Gabriel Carvalho Mota <gabrielcarvalhomota05@gmail.com>
2025-03-19 22:23:26 -0300

    #18
    Criação da tela de login FrontEnd

--- app/src/main/java/br/com/fecapccp/uber_saferide/TelaLogin.java
+++ app/src/main/java/br/com/fecapccp/uber_saferide/TelaLogin.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +24,16 @@ public class TelaLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void onInputClick(View view) {
+        Toast.makeText(this, "Campo selecionado!", Toast.LENGTH_SHORT).show();
+    }
 
+    public void onLoginClick(View view) {
+        Toast.makeText(this, "Botão de Login pressionado!", Toast.LENGTH_SHORT).show();
+    }
 
+    public void onCriarContaClick(View view) {
+        Toast.makeText(this, "Criar Conta clicado!", Toast.LENGTH_SHORT).show();
     }
 }
