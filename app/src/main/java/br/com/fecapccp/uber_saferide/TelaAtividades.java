f855c71887a390542611424456aa74e240cee4fd
Guilherme Siqueira <134648341+SIkas000@users.noreply.github.com>
2025-03-21 21:24:11 -0300

    FrontEnd (Passageiro)
    FrontEnd da tela de atividades/Histórico

--- app/src/main/java/br/com/fecapccp/uber_saferide/TelaAtividades.java
+++ app/src/main/java/br/com/fecapccp/uber_saferide/TelaAtividades.java
@@ -1,8 +1,11 @@
 package br.com.fecapccp.uber_saferide;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,5 +30,24 @@ public class TelaAtividades extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView imgPerfil = findViewById(R.id.imgPerfil);
+        imgPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaAtividades.this, ConfiguracaoPerfil.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imgHome = findViewById(R.id.imgHome);
+        imgHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaAtividades.this, IniciarViagem.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
