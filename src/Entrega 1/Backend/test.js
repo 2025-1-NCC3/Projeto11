a66d2617c4ef68ae4ab69e9210e55aec952d101b
Rodrigo Luiz <rluizreis.rodrigo@gmail.com>
2025-03-25 23:25:21 -0300

    removinig test.js

--- src/Entrega 1/Backend/test.js
+++
@@ -1,5 +0,0 @@
-const { deleteUser } = require('./controllers/usuarioController');
-
-deleteUser({ body: { id: 7 } }, { 
-    status: (code) => ({ json: (msg) => console.log(`Status: ${code}`, msg) }) 
-});
