95c0173b943fb58f7d8a622c3f9850c050aa387c
Rodrigo Luiz <rluizreis.rodrigo@gmail.com>
2025-04-23 10:14:26 -0300

    changing base url to new api one

--- src/Entrega 2/Frontend/appPassageiro/src/main/java/br/com/fecapccp/uber_saferide/retrofit/RetrofitClient.java
+++ src/Entrega 2/Frontend/appPassageiro/src/main/java/br/com/fecapccp/uber_saferide/retrofit/RetrofitClient.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import java.util.concurrent.TimeUnit;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://rzk96n-5000.csb.app/";
+    private static final String BASE_URL = "https://fkhx9k-5000.csb.app";
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
