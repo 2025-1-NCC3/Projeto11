9f26ea5612516dde9ee3e60a8e5d7f5a80fb3a09
Rodrigo Luiz <rluizreis.rodrigo@gmail.com>
2025-05-01 18:38:48 -0300

    changing url to backend

--- src/Entrega 3/Frontend/appMotorista/src/main/java/br/fecap/pi/saferide_motorista/retrofit/RetrofitClient.java
+++ src/Entrega 3/Frontend/appMotorista/src/main/java/br/fecap/pi/saferide_motorista/retrofit/RetrofitClient.java
@@ -11,7 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://fkhx9k-5000.csb.app";
+    private static final String BASE_URL = "https://crk7zm-5000.csb.app";
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
