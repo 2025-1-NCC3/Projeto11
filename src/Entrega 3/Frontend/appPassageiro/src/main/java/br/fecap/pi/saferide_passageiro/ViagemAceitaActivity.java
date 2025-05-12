5e985bb72ae871585d62f34c7da43b3c63cd8b7c
Guilherme Siqueira <134648341+SIkas000@users.noreply.github.com>
2025-05-12 19:43:45 -0300

    ajustes

--- src/Entrega 3/Frontend/appPassageiro/src/main/java/br/fecap/pi/saferide_passageiro/ViagemAceitaActivity.java
+++ src/Entrega 3/Frontend/appPassageiro/src/main/java/br/fecap/pi/saferide_passageiro/ViagemAceitaActivity.java
@@ -13,37 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ViagemAceitaActivity extends AppCompatActivity {
-
-    private Button btnTViagemAceitaSet;
-    private Button btnCancelarViagem;
-    private Button btnMudarViagem;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_viagem_aceita);
-
-        btnTViagemAceitaSet = findViewById(R.id.btnTViagemAceitaSet);
-        btnCancelarViagem = findViewById(R.id.btnCancelarViagem);
-        btnMudarViagem = findViewById(R.id.btnMudarViagem);
-
-
-        btnTViagemAceitaSet.setOnClickListener(view -> {
-            Intent intent = new Intent(this, MetodoPagamento.class);
-            startActivity(intent);
-        });
-
-        btnCancelarViagem.setOnClickListener(view -> {
-            finish();
-        });
-
-        btnMudarViagem.setOnClickListener(view -> {
-            Intent intent = new Intent(this, IniciarViagem.class);
-            startActivity(intent);
-        });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
